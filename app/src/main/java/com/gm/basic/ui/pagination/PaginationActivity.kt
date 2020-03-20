7e00c45e4682bf7f8189e4b36e4539569503a046
Gowtham <goutham.gm11@gmail.com>
2020-03-20 16:09:36 +0530

    Pagination Started

---
+++ app/src/main/java/com/gm/basic/ui/pagination/PaginationActivity.kt
@@ -0,0 +1,13 @@
+package com.gm.basic.ui.pagination
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gm.basic.R
+
+class PaginationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagination)
+    }
+}
--- app/src/main/java/com/gm/basic/utils/Utils.kt
+++ app/src/main/java/com/gm/basic/utils/Utils.kt
@@ -2,12 +2,14 @@ package com.gm.basic.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import android.util.Log
 import com.gm.basic.domain.Api
 import com.gm.basic.room.AppDatabase
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -30,6 +32,15 @@ object Utils {
             .build()
     }
 
+    private fun generatePagingRetrofit(context: Context): Retrofit {
+        return Retrofit.Builder()
+            .client(generateOkHttpClient(context))
+            .baseUrl("https://testapi.io/api/crosscodedj/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
+
 
     fun getApiService(context: Context): Api {
         return generateRetrofit(context).create(Api::class.java)
@@ -42,30 +53,35 @@ object Utils {
 
     private fun generateOkHttpClient(context: Context): OkHttpClient {
         val cacheSize = (5 * 1024 * 1024).toLong() // 5MB
+
+        val CACHE_CONTROL_INTERCEPTOR: Interceptor = Interceptor { chain ->
+            var originalResponse = chain.proceed(chain.request())
+            if (hasNetwork(context)) {
+                val maxAge = 60 // read from cache for 1 minute
+                originalResponse.newBuilder()
+                    .header("Cache-Control", "public, max-age=$maxAge")
+                    .build()
+            } else {
+                val maxStale = 60 * 60 * 24 * 7 // tolerate 1-weeks stale
+                originalResponse.newBuilder().header(
+                    "Cache-Control",
+                    "public, only-if-cached, max-stale=$maxStale"
+                ).build()
+            }
+
+        }
+
         val myCache = Cache(context.cacheDir, cacheSize)
 
         return OkHttpClient.Builder()
-            .cache(myCache)
+            .addNetworkInterceptor(CACHE_CONTROL_INTERCEPTOR)
             .addInterceptor(getHttpLoggingInterceptor())
-            .addInterceptor { chain ->
-                var request = chain.request()
-                request = if (hasNetwork(context)!!)
-                    request.newBuilder().header(
-                        "Cache-Control",
-                        "public, max-age=" + 5
-                    ).build()
-                else
-                    request.newBuilder().header(
-                        "Cache-Control",
-                        "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7
-                    ).build()
-                chain.proceed(request)
-            }
+            .cache(myCache)
             .build()
 
     }
 
-    fun hasNetwork(context: Context): Boolean {
+    fun hasNetworkOld(context: Context): Boolean {
         var isConnected: Boolean = false // Initial Value
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -75,7 +91,26 @@ object Utils {
         return isConnected
     }
 
-    fun getHttpLoggingInterceptor(): HttpLoggingInterceptor {
+    fun hasNetwork(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork
+            val connection = connectivityManager.getNetworkCapabilities(network)
+            return connection != null && (
+                    connection.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                            connection.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+        } else {
+            val activeNetwork = connectivityManager.activeNetworkInfo
+            if (activeNetwork != null) {
+                return (activeNetwork.type == ConnectivityManager.TYPE_WIFI ||
+                        activeNetwork.type == ConnectivityManager.TYPE_MOBILE)
+            }
+            return false
+        }
+    }
+
+    private fun getHttpLoggingInterceptor(): HttpLoggingInterceptor {
         return HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
             override fun log(message: String) {
                 Log.e("HttpLoggingInterceptor", message)
